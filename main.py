c97362b3d5df86fc5f2323ee6764983a562c5e15
AlexSalfetka <alesssaug@gmail.com>
2025-04-16 04:16:30 +1000

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
-from tgb import bot  # Импортируем объект бота из bot_logic.py
+from tgb import bot
+from flask import Flask
+import threading
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET"])
+def index():
+    return "Bot is alive!"
+
+def start_bot():
+    bot.infinity_polling()
 
 if __name__ == "__main__":
-    bot.polling(non_stop=True)  # Запускаем бота, чтобы он начал обрабатывать сообщения
+    threading.Thread(target=start_bot).start()
+    app.run(host="0.0.0.0", port=10000)
